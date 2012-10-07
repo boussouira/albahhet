26ba1af32c74b323b7e468c0d153dd0817fa40ec
Ahmed Saidi <justroftest@gmail.com>
2012-10-07 16:33:56 +0000

    New release

--- src/app_version.h
+++ src/app_version.h
@@ -1,8 +1,8 @@
 #ifndef APP_VERSION_H
 #define APP_VERSION_H
 
-#define APP_VERSION 0x010301
-#define APP_VERSION_STR QObject::tr("1.3.1")
+#define APP_VERSION 0x010302
+#define APP_VERSION_STR QObject::tr("1.3.2")
 #define APP_NAME QObject::tr("الباحث الشامل")
 
 #endif // APP_VERSION_H
